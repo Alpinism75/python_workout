db5d2927f44f5f8fa86f3123cacd208abef924c4
Alpinism75 <56472609+Alpinism75@users.noreply.github.com>
2021-02-01 07:15:34 +0000

    second exercise - working

--- chtp1_exercise2.py
+++ chtp1_exercise2.py
@@ -5,29 +5,30 @@ However, instead of taking a single sequence as a parameter, it should take a va
 Thus, although you might invoke sum([1,2,3]), you’d instead invoke mysum(1,2,3) or mysum(10,20,30,40,50).
 """
 
-def my_sum(**args):
-    params = []
+def my_sum(*argsv):
+    params = argsv[0]
     f = 0
 
-    for x in args:
-        params.append(x)
+    for x in params:
+        int(x)
         f += x
     
     print(f)
 
 
 def input_value(): 
-
     all = []
-
     while True:
-        add = input("input all the numbers (no number to quit): ")
+        add = input("Input all the numbers (no number to add, 'q' to quit): ")
         try:
             all.append(int(add))
         except:
-            print("No Number received so we'll add all numbers so far")
-            break
-    my_sum(all)
+            if add == 'q':
+                break
+            else:
+                print("No Number received so we'll add all numbers so far")
+                my_sum(all)
+                all =[]
 
 if __name__ == "__main__":
     input_value()
